6ebb462a8fc4e2cc347f0b23087782a8aee8daad
le0apolinari0 <leosantosap@outlook.com>
2024-03-06 15:11:37 -0300

    Novo menu aumentando as funcionalidades de busca

--- src/main/java/br/com/apicarros/ApiTabelaFipe/ApiTabelaFipeApplication.java
+++ src/main/java/br/com/apicarros/ApiTabelaFipe/ApiTabelaFipeApplication.java
@@ -17,6 +17,7 @@ public class ApiTabelaFipeApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Principal principal = new Principal();
 		principal.exibeMenu();
+		principal.menu2();
 	}
 }
 
